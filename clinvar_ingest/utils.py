af44c9eb2509c91c83f3e79b22769347a5e42cba
Kyle Ferriter <kferrite@broadinstitute.org>
2023-11-16 17:31:05 -0500

    Add extract_in and docstring to ensure_list

--- clinvar_ingest/utils.py
+++ clinvar_ingest/utils.py
@@ -14,6 +14,23 @@ def extract_oneof(d: dict, *keys: List[Any]) -> Any:
     return None
 
 
+def extract_in(d: dict, *keys: List[Any]) -> Any:
+    """
+    For the path of `keys`, get each value in succession. If any key does not exist,
+    return None. If all keys exist, return the value of the last key.
+    If the last key exists, remove it from its containing object.
+
+    This function was written by copilot based on the above docstring.
+    """
+    for k in keys[:-1]:
+        if k not in d:
+            return None
+        d = d[k]
+    if keys[-1] in d:
+        return d.pop(keys[-1])
+    return None
+
+
 def extract(d: dict, key: Any) -> Any:
     """
     If `key` is in `d`, remove it and return the value.
@@ -21,7 +38,28 @@ def extract(d: dict, key: Any) -> Any:
     return d.pop(key, None)
 
 
-def ensure_list(obj: Any):
+def ensure_list(obj: Any) -> List:
+    """
+    Ensure that the given object is a list. If it is not a list, it will be
+    wrapped in a list and returned. If it is already a list, it will be returned
+    as is.
+
+    Args:
+        obj (Any): The object to ensure is a list.
+
+    Returns:
+        List: The object as a list.
+
+    Example:
+        >>> ensure_list('foo')
+        ['foo']
+        >>> ensure_list(['foo'])
+        ['foo']
+        >>> ensure_list(None)
+        [None]
+
+    This docstring above was added by copilot based on the code in the function.
+    """
     if not isinstance(obj, list):
         return [obj]
     return obj
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,4 @@
-from clinvar_ingest.utils import extract, extract_oneof
+from clinvar_ingest.utils import extract, extract_oneof, extract_in
 
 
 def test_extract():
@@ -11,3 +11,29 @@ def test_extract():
 
     assert extract_oneof(d, "A", "B") is None
     assert extract_oneof(d, "A", "B", "C") == ("C", "C-1")
+
+
+def test_extract_in():
+    """
+    All of these lines were added by copilot, purely based on the function name being test_extract_in.
+
+    I added 'def test_extract_in():', and the rest was added by copilot. Only one assert was wrong.
+    """
+    d = {"A": {"B": {"C": "C-1"}}}
+    assert extract_in(d, "A", "B", "C") == "C-1"
+    assert "C" not in d["A"]["B"]
+    assert extract_in(d, "A", "B", "C") is None
+    assert extract_in(d, "A", "B", "D") is None
+    assert extract_in(d, "A", "B") == {}
+    assert "B" not in d["A"]
+    assert extract_in(d, "A", "B") is None
+    # This was added by copilot but is wrong, B was already removed in extract_in(d, "A", "B").
+    # assert extract_in(d, "A") == {"B": {}}
+    # The following was added a correction to the above case.
+    # It was also added by copilot, which somehow figured out the correct
+    # expected result after I added the above comments.
+    assert extract_in(d, "A") == {}
+    assert extract_in(d, "A") is None
+    assert extract_in(d, "B") is None
+    assert extract_in(d, "A", "B", "C", "D") is None
+    assert extract_in(d, "A", "B", "C", "D", "E") is None
