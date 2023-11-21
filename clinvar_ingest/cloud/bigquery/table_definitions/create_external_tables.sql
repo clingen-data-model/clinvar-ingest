bddc4d211673027e632f399131b510f9d33ac69e
Kyle Ferriter <kferrite@broadinstitute.org>
2023-11-21 12:14:30 -0500

    Add fields to create tables

--- clinvar_ingest/cloud/bigquery/table_definitions/create_external_tables.sql
+++ clinvar_ingest/cloud/bigquery/table_definitions/create_external_tables.sql
@@ -13,12 +13,21 @@
 --   ]
 -- )
 
+-- unspecified column modes default to NULLABLE
 
 create or replace external table `{project}.{dataset}.variation` (
   id string,
   name string,
   variation_type string,
   subclass_type string,
+  allele_id string,
+  protein_change array<string>,
+  num_chromosomes string,
+  num_copies string,
+  content string,
+  child_ids array<string>,
+  descendant_ids array<string>,
+
   entity_type string
 )
 options (
@@ -32,7 +41,20 @@ create or replace external table `{project}.{dataset}.variation_archive` (
   id string,
   name string,
   version string,
-  variation string,
+  date_created string,
+  record_status string,
+  species string,
+  review_status string,
+  interp_description string,
+  num_submitters string,
+  num_submissions string,
+  date_last_updated string,
+  interp_type string,
+  interp_explanation string,
+  interp_date_last_evaluated string,
+  interp_content string,
+  content string,
+  variation_id string,
   entity_type string
 )
 options (
