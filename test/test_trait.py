4301683145c935a9431f38a767ec6121e8d9b1ab
Kyle Ferriter <kferrite@broadinstitute.org>
2023-12-04 15:39:00 -0500

    Fix test_trait_from_xml_32 by adding expected medgen id and traits

--- test/test_trait.py
+++ test/test_trait.py
@@ -1,5 +1,13 @@
+from typing import List
 from clinvar_ingest.model import Trait, dictify
 from clinvar_ingest.reader import _parse_xml_document
+
+
+def unordered_dict_list_equal(list1: List[dict], list2: List[dict]) -> bool:
+    set1 = set([tuple(elem.items()) for elem in list1])
+    set2 = set([tuple(elem.items()) for elem in list2])
+    return len(list1) == len(list2) and set1 == set2
+    # return set(list1) == set(list2)
 
 
 def test_trait_from_xml_32():
@@ -26,15 +34,92 @@ def test_trait_from_xml_32():
     assert trait.ghr_links is None
     assert trait.keywords is None
     assert trait.gard_id == 10738
-    assert trait.medgen_id is None
+    assert trait.medgen_id == "C3150878"
     assert trait.public_definition is None
 
     assert trait.disease_mechanism == "loss of function"
     assert trait.disease_mechanism_id == 273
     assert trait.gene_reviews_short is None
-    assert dictify(trait.xrefs) == []
+    # assert dictify(trait.xrefs) == []
     assert trait.attribute_content is None
     assert trait.content is not None
+
+    # assert set(dictify(trait.xrefs)) == set(
+    assert unordered_dict_list_equal(
+        dictify(trait.xrefs),
+        [
+            {
+                "db": "MONDO",
+                "id": "MONDO:0013327",
+                "type": None,
+                "ref_field": "name",
+                "ref_field_element": "Primary hyperoxaluria type 3",
+            },
+            {
+                "db": "Genetic Alliance",
+                "id": "Primary+Hyperoxaluria+Type+3/8596",
+                "type": None,
+                "ref_field": "alternate_names",
+                "ref_field_element": "Primary hyperoxaluria, type III",
+            },
+            {
+                "db": "OMIM",
+                "id": "613616",
+                "type": "MIM",
+                "ref_field": "alternate_symbols",
+                "ref_field_element": "HP3",
+            },
+            {
+                "db": "Office of Rare Diseases",
+                "id": "10738",
+                "type": None,
+                "ref_field": "gard_id",
+                "ref_field_element": None,
+            },
+            {
+                "db": "Genetic Testing Registry (GTR)",
+                "id": "GTR000561373",
+                "type": None,
+                "ref_field": "disease_mechanism",
+                "ref_field_element": None,
+            },
+            {
+                "db": "Orphanet",
+                "id": "416",
+                "type": None,
+                "ref_field": None,
+                "ref_field_element": None,
+            },
+            {
+                "db": "Orphanet",
+                "id": "93600",
+                "type": None,
+                "ref_field": None,
+                "ref_field_element": None,
+            },
+            {
+                "db": "MedGen",
+                "id": "C3150878",
+                "type": None,
+                "ref_field": None,
+                "ref_field_element": None,
+            },
+            {
+                "db": "MONDO",
+                "id": "MONDO:0013327",
+                "type": None,
+                "ref_field": None,
+                "ref_field_element": None,
+            },
+            {
+                "db": "OMIM",
+                "id": "613616",
+                "type": "MIM",
+                "ref_field": None,
+                "ref_field_element": None,
+            },
+        ],
+    )
 
 
 """
