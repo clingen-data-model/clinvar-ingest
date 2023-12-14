2f23adc1ee42ed2da2f33fa44e5dc6ba04ae2549
Kyle Ferriter <kferrite@broadinstitute.org>
2023-12-14 01:19:03 -0500

    Add ParseResponse validation

--- clinvar_ingest/api/model/requests.py
+++ clinvar_ingest/api/model/requests.py
@@ -1,6 +1,15 @@
 from datetime import date, datetime
+from typing import Union
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import (
+    AnyUrl,
+    BaseModel,
+    ConfigDict,
+    Field,
+    FilePath,
+    RootModel,
+    validator,
+)
 
 
 def to_title_case(string: str) -> str:
@@ -33,16 +42,37 @@ class ClinvarFTPWatcherRequest(BaseModel):
     release_date: date
 
 
-class ParseRequest(BaseModel):
+class CopyResponse(BaseModel):
     ftp_path: str
     gcs_path: str
+
+
+class ParseRequest(BaseModel):
+    input_path: str
+    output_path: str
     no_disassemble: bool = Field(default=True)
     no_jsonify_content: bool = Field(default=True)
 
 
-class CopyResponse(BaseModel):
-    ftp_path: str
-    gcs_path: str
+class GCSBlobPath(RootModel):
+    """
+    A GCS blob path, such as gs://my-bucket/my-file.txt
+    Validates path structure, does not check if the file exists.
+    """
+
+    root: str
+
+    @validator("root")
+    def _validate(cls, v):  # pylint: disable=E0213
+        if not v.startswith("gs://"):
+            raise ValueError(f"Must be a gs:// URL: {v}")
+        AnyUrl(v)
+        return v
+
+
+class ParseResponse(BaseModel):
+    # Either URLs (such as gs:// URLs) or paths to local files which exist
+    parsed_files: list[Union[GCSBlobPath, FilePath]]
 
 
 class TodoRequest(BaseModel):  # A shim to get the workflow pieced together
