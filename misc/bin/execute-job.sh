d8a200ba189641cf939edf072018aa6a61b34825
Kyle Ferriter <kferrite@broadinstitute.org>
2024-02-21 17:07:43 -0500

    Make job execution wait in github action

--- misc/bin/execute-job.sh
+++ misc/bin/execute-job.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -xeo pipefail
+
+if [ -z "$branch" ]; then
+    branch=$(git rev-parse --abbrev-ref HEAD)
+else
+    echo "branch set in environment"
+fi
+
+if [ "$JOB_WAIT" == "1" ]; then
+    wait_opt="--wait"
+else
+    wait_opt="--async" # the default
+fi
+
+set -u
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 commit=$(git rev-parse HEAD)
@@ -25,13 +39,13 @@ release_date="2024-02-01"
 
 
 # Smaller input
-# host=https://raw.githubusercontent.com
-# directory=/clingen-data-model/clinvar-ingest/main/test/data
-# name=OriginalTestDataSet.xml.gz
-# size=46719
-# last_modified=2023-10-07T15:47:16
-# released=2023-10-07T15:47:16
-# release_date=2023-10-07
+host=https://raw.githubusercontent.com
+directory=/clingen-data-model/clinvar-ingest/main/test/data
+name=OriginalTestDataSet.xml.gz
+size=46719
+last_modified=2023-10-07T15:47:16
+released=2023-10-07T15:47:16
+release_date=2023-10-07
 
 
 env_vars="CLINVAR_INGEST_BUCKET=$CLINVAR_INGEST_BUCKET"
@@ -43,7 +57,7 @@ env_vars="$env_vars,last_modified=$last_modified"
 env_vars="$env_vars,released=$released"
 env_vars="$env_vars,release_date=$release_date"
 
-
 gcloud run jobs execute $job_name \
     --region $region \
+    $wait_opt \
     --update-env-vars=$env_vars
