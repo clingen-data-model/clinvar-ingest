e9f4a54e561dc13b9c2738c02dfbb82e3024bb53
Kyle Ferriter <kferrite@broadinstitute.org>
2024-06-05 12:08:19 -0400

    Add tests for clinical_assertion_variation generated ids and child and descendant aggregation

--- test/test_variation.py
+++ test/test_variation.py
@@ -1,6 +1,6 @@
 import xmltodict
 
-from clinvar_ingest.model.variation_archive import Variation
+from clinvar_ingest.model.variation_archive import ClinicalAssertionVariation, Variation
 
 
 def test_variation_descendant_tree():
@@ -46,3 +46,119 @@ def test_variation_get_all_descendants():
     descendants = Variation.get_all_descendants(descendant_tree)
     expected_descendants = ["633847", "633853", "634864", "634875", "634882", "633853"]
     assert expected_descendants == descendants
+
+
+def test_clinical_assertion_variation_descendants():
+    with open("test/data/original-clinvar-variation-2.xml") as inp:
+        inp_xml = inp.read()
+
+    inp = xmltodict.parse(inp_xml)
+    interp_record = inp["ClinVarVariationRelease"]["VariationArchive"][
+        "InterpretedRecord"
+    ]
+    clinical_assertion_list = interp_record["ClinicalAssertionList"][
+        "ClinicalAssertion"
+    ]
+    assert isinstance(clinical_assertion_list, list)
+    assert len(clinical_assertion_list) == 2
+
+    scv0_variations = ClinicalAssertionVariation.extract_variations(
+        clinical_assertion_list[0],
+        clinical_assertion_list[0]["ClinVarAccession"]["@Accession"],
+    )
+    assert len(scv0_variations) == 1
+    v0: ClinicalAssertionVariation = scv0_variations[0]
+    assert v0.id == "SCV000020155.0"
+    assert v0.content["Name"] == "AP5Z1, 4-BP DEL/22-BP INS, NT80"
+    assert v0.clinical_assertion_id == "SCV000020155"
+    assert v0.child_ids == []
+    assert v0.descendant_ids == []
+    # print(scv0_variations[0])
+
+
+def test_clinical_assertion_variation_descendants_genotype():
+    with open("test/data/original-clinvar-variation-634266.xml") as inp:
+        inp_xml = inp.read()
+
+    inp = xmltodict.parse(inp_xml)
+    interp_record = inp["ClinVarVariationRelease"]["VariationArchive"][
+        "InterpretedRecord"
+    ]
+    clinical_assertion_list = interp_record["ClinicalAssertionList"][
+        "ClinicalAssertion"
+    ]
+    assert isinstance(clinical_assertion_list, list)
+    assert len(clinical_assertion_list) == 4
+
+    scv000921753 = clinical_assertion_list[0]
+    assert scv000921753["ClinVarAccession"]["@Accession"] == "SCV000921753"
+
+    scv000921753_variations = ClinicalAssertionVariation.extract_variations(
+        scv000921753,
+        "SCV000921753",
+    )
+    assert len(scv000921753_variations) == 7
+    genotype: ClinicalAssertionVariation = scv000921753_variations[0]
+    assert genotype.subclass_type == "Genotype"
+    assert genotype.id == "SCV000921753.0"
+
+    haplotypeA: ClinicalAssertionVariation = scv000921753_variations[1]
+    assert haplotypeA.subclass_type == "Haplotype"
+    assert haplotypeA.id == "SCV000921753.1"
+
+    simplealleleAA: ClinicalAssertionVariation = scv000921753_variations[2]
+    assert simplealleleAA.subclass_type == "SimpleAllele"
+    assert simplealleleAA.id == "SCV000921753.2"
+
+    haplotypeB: ClinicalAssertionVariation = scv000921753_variations[3]
+    assert haplotypeB.subclass_type == "Haplotype"
+    assert haplotypeB.id == "SCV000921753.3"
+
+    simplealleleBA: ClinicalAssertionVariation = scv000921753_variations[4]
+    assert simplealleleBA.subclass_type == "SimpleAllele"
+    assert simplealleleBA.id == "SCV000921753.4"
+
+    simplealleleBB: ClinicalAssertionVariation = scv000921753_variations[5]
+    assert simplealleleBB.subclass_type == "SimpleAllele"
+    assert simplealleleBB.id == "SCV000921753.5"
+
+    simplealleleBC: ClinicalAssertionVariation = scv000921753_variations[6]
+    assert simplealleleBC.subclass_type == "SimpleAllele"
+    assert simplealleleBC.id == "SCV000921753.6"
+
+    # Check direct children
+    assert genotype.child_ids == [haplotypeA.id, haplotypeB.id]
+    assert haplotypeA.child_ids == [simplealleleAA.id]
+    assert simplealleleAA.child_ids == []
+    assert haplotypeB.child_ids == [
+        simplealleleBA.id,
+        simplealleleBB.id,
+        simplealleleBC.id,
+    ]
+    assert simplealleleBA.child_ids == []
+    assert simplealleleBB.child_ids == []
+    assert simplealleleBC.child_ids == []
+
+    # Check descendants
+    assert list(sorted(genotype.descendant_ids)) == list(
+        sorted(
+            [
+                haplotypeA.id,
+                simplealleleAA.id,
+                haplotypeB.id,
+                simplealleleBA.id,
+                simplealleleBB.id,
+                simplealleleBC.id,
+            ]
+        )
+    )
+    assert haplotypeA.descendant_ids == [simplealleleAA.id]
+    assert simplealleleAA.descendant_ids == []
+    assert haplotypeB.descendant_ids == [
+        simplealleleBA.id,
+        simplealleleBB.id,
+        simplealleleBC.id,
+    ]
+    assert simplealleleBA.descendant_ids == []
+    assert simplealleleBB.descendant_ids == []
+    assert simplealleleBC.descendant_ids == []
