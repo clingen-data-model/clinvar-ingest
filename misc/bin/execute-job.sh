8d41aa60e07e23e7ee890a60f9c87ea03c11859e
Kyle Ferriter <kferrite@broadinstitute.org>
2024-07-18 11:58:26 -0400

    Add rcv auto-deploy to release github action. Let caller choose vcv or rcv small file in execute-job.sh

--- misc/bin/execute-job.sh
+++ misc/bin/execute-job.sh
@@ -34,47 +34,78 @@ region="us-east1"
 # Global Variables
 CLINVAR_INGEST_BUCKET="clinvar-ingest-dev"
 
+# Default input
+if [ -z "$1" ]; then
+    input_flag="vcv-small"
+else
+    input_flag=$1
+fi
 
-# Larger input
-host="https://ftp.ncbi.nlm.nih.gov"
-directory="/pub/clinvar/xml/VCV_xml_old_format"
-name="ClinVarVariationRelease_2024-02.xml.gz"
-size=3298023159
-last_modified="2024-01-07T15:47:16"
-released="2024-02-01T15:47:16"
-release_date="2024-02-01"
-file_format=vcv
+if [ "$input_flag" == "vcv-small" ]; then
+    host="https://raw.githubusercontent.com"
+    directory="/clingen-data-model/clinvar-ingest/main/test/data"
+    name="OriginalTestDataSet.xml.gz"
+    size=46719
+    last_modified="2023-10-07T15:47:16"
+    released="2023-10-07T15:47:16"
+    release_date="2023-10-07"
+    file_format="vcv"
+elif [ "$input_flag" == "rcv-small" ]; then
+    # https://raw.githubusercontent.com/clingen-data-model/clinvar-ingest/issue-187-rcv-workflow/test/data/rcv/combined.xml.gz
+    host="https://raw.githubusercontent.com"
+    directory="/clingen-data-model/clinvar-ingest/main/test/data/rcv"
+    name=combined.xml.gz
+    size=5220
+    last_modified="2024-06-10T12:00:00"
+    released="2024-06-10T12:00:00"
+    release_date="2024-06-10"
+    file_format="rcv"
+else
+    echo "Invalid input flag"
+    exit 1
+fi
 
-# Larger input 2024-06-03
-host="https://ftp.ncbi.nlm.nih.gov"
-directory="/pub/clinvar/xml/VCV_xml_old_format"
-name=ClinVarVariationRelease_2024-0603.xml.gz
-size=3910466672
-last_modified='2024-06-04T09:09:59'
-released='2024-06-04T09:09:59'
-release_date='2024-06-03'
-file_format=vcv
 
-# Smaller input
-host=https://raw.githubusercontent.com
-directory=/clingen-data-model/clinvar-ingest/main/test/data
-name=OriginalTestDataSet.xml.gz
-size=46719
-last_modified=2023-10-07T15:47:16
-released=2023-10-07T15:47:16
-release_date=2023-10-07
-file_format=vcv
+# # Larger input
+# host="https://ftp.ncbi.nlm.nih.gov"
+# directory="/pub/clinvar/xml/VCV_xml_old_format"
+# name="ClinVarVariationRelease_2024-02.xml.gz"
+# size=3298023159
+# last_modified="2024-01-07T15:47:16"
+# released="2024-02-01T15:47:16"
+# release_date="2024-02-01"
+# file_format=vcv
 
-# RCV 2024-06-10 input in GCS
-# gs://clinvar-ingest/test-data/ClinVarRCVRelease_2024-0610.xml.gz
-host=gs://clinvar-ingest
-directory=/test-data
-name=ClinVarRCVRelease_2024-0610.xml.gz
-size=4342574098
-last_modified=2024-06-10T12:00:00
-released=2024-06-10T12:00:00
-release_date=2024-06-10
-file_format=rcv
+# # Larger input 2024-06-03
+# host="https://ftp.ncbi.nlm.nih.gov"
+# directory="/pub/clinvar/xml/VCV_xml_old_format"
+# name=ClinVarVariationRelease_2024-0603.xml.gz
+# size=3910466672
+# last_modified='2024-06-04T09:09:59'
+# released='2024-06-04T09:09:59'
+# release_date='2024-06-03'
+# file_format=vcv
+
+# # Smaller input
+# host=https://raw.githubusercontent.com
+# directory=/clingen-data-model/clinvar-ingest/main/test/data
+# name=OriginalTestDataSet.xml.gz
+# size=46719
+# last_modified=2023-10-07T15:47:16
+# released=2023-10-07T15:47:16
+# release_date=2023-10-07
+# file_format=vcv
+
+# # RCV 2024-06-10 input in GCS
+# # gs://clinvar-ingest/test-data/ClinVarRCVRelease_2024-0610.xml.gz
+# host=gs://clinvar-ingest
+# directory=/test-data
+# name=ClinVarRCVRelease_2024-0610.xml.gz
+# size=4342574098
+# last_modified=2024-06-10T12:00:00
+# released=2024-06-10T12:00:00
+# release_date=2024-06-10
+# file_format=rcv
 
 # Small test input in GCS
 # host=gs://clinvar-ingest
