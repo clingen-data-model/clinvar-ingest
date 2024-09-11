ebdb946b89c33caa400b6a8511ff5a15c26a27e2
Kyle Ferriter <kferrite@broadinstitute.org>
2024-09-11 16:15:51 -0400

    Add handling of RCV Classification model

--- test/clinvar_somatic/test_parse.py
+++ test/clinvar_somatic/test_parse.py
@@ -1,7 +1,10 @@
 from clinvar_ingest.model.common import dictify
 from clinvar_ingest.model_somatic.variation_archive import (
+    ClinicalAssertionSomatic,
+    RcvAccessionSomatic,
     StatementType,
     VariationArchiveClassification,
+    VariationArchiveSomatic,
 )
 from clinvar_ingest.reader import read_clinvar_somatic_model_vcv_xml
 
@@ -27,5 +30,110 @@ def test_vcv_VCV000000002():
     assert vcv_classification.num_submitters == 2
     assert vcv_classification.review_status == "criteria provided, single submitter"
     assert vcv_classification.statement_type == StatementType.GermlineClassification
+
+    # VariationArchive
+    vcv = [o for o in objects if isinstance(o, VariationArchiveSomatic)]
+    assert len(vcv) == 1
+    vcv = vcv[0]
+    assert vcv.entity_type == "variation_archive_somatic"
+    assert vcv.date_created == "2017-01-30"
+    assert vcv.date_last_updated == "2022-04-25"
+    assert vcv.id == "VCV000000002"
+    assert (
+        vcv.name
+        == "NM_014855.3(AP5Z1):c.80_83delinsTGCTGTAAACTGTAACTGTAAA (p.Arg27_Ile28delinsLeuLeuTer)"
+    )
+    assert vcv.num_submissions == 2
+    assert vcv.num_submitters == 2
+    assert vcv.record_status == "current"
+    assert vcv.species == "Homo sapiens"
+    assert vcv.variation_id == "2"
+    assert vcv.version == "3"
+
     print(dictify(vcv_classification))
-    # assert False
+
+    # ClinicalAssertion
+    scv = [o for o in objects if isinstance(o, ClinicalAssertionSomatic)]
+    assert len(scv) == 2
+    scv20155 = scv[0]
+    scv2865972 = scv[1]
+
+    # SCV scv20155
+    assert scv20155.assertion_type == "variation to disease"
+    assert scv20155.clinical_impact_assertion_type is None
+    assert scv20155.clinical_impact_clinical_significance is None
+    assert scv20155.date_created == "2013-04-04"
+    assert scv20155.date_last_updated == "2017-01-30"
+    assert scv20155.entity_type == "clinical_assertion_somatic"
+    assert scv20155.id == "SCV000020155"
+    assert scv20155.internal_id == "20155"
+    assert scv20155.interpretation_comments == []
+    assert scv20155.interpretation_date_last_evaluated == "2010-06-29"
+    assert scv20155.interpretation_description is None
+    assert (
+        scv20155.local_key == "613653.0001_SPASTIC PARAPLEGIA 48, AUTOSOMAL RECESSIVE"
+    )
+    assert scv20155.record_status == "current"
+    assert scv20155.review_status == "no assertion criteria provided"
+    assert scv20155.statement_type == StatementType.GermlineClassification
+    assert scv20155.submission_id == "3.2017-01-26"
+    assert scv20155.submission_names == []
+    assert scv20155.submitted_assembly is None
+    assert scv20155.submitter_id == "3"
+    assert (
+        scv20155.title
+        == "AP5Z1, 4-BP DEL/22-BP INS, NT80_SPASTIC PARAPLEGIA 48, AUTOSOMAL RECESSIVE"
+    )
+    assert scv20155.variation_archive_id == "VCV000000002"
+    assert scv20155.variation_id == "2"
+    assert scv20155.version == "3"
+
+    # SCV scv2865972
+    assert scv2865972.assertion_type == "variation to disease"
+    assert scv2865972.clinical_impact_assertion_type is None
+    assert scv2865972.clinical_impact_clinical_significance is None
+    assert scv2865972.date_created == "2021-05-16"
+    assert scv2865972.date_last_updated == "2021-05-16"
+    assert scv2865972.entity_type == "clinical_assertion_somatic"
+    assert scv2865972.id == "SCV001451119"
+    assert scv2865972.internal_id == "2865972"
+    assert scv2865972.interpretation_comments == []
+    assert scv2865972.interpretation_date_last_evaluated is None
+    assert scv2865972.interpretation_description is None
+    assert (
+        scv2865972.local_key
+        == "NM_014855.3:c.80_83delinsTGCTGTAAACTGTAACTGTAAA|OMIM:613647"
+    )
+    assert scv2865972.record_status == "current"
+    assert scv2865972.review_status == "criteria provided, single submitter"
+    assert scv2865972.statement_type == StatementType.GermlineClassification
+    assert scv2865972.submission_id == "507826.2020-11-14"
+    assert scv2865972.submission_names == ["SUB8526155"]
+    assert scv2865972.submitted_assembly == "GRCh37"
+    assert scv2865972.submitter_id == "507826"
+    assert scv2865972.title is None
+    assert scv2865972.variation_archive_id == "VCV000000002"
+    assert scv2865972.variation_id == "2"
+    assert scv2865972.version == "1"
+
+    # RCV
+    rcvs = [o for o in objects if isinstance(o, RcvAccessionSomatic)]
+    assert len(rcvs) == 1
+    rcv: RcvAccessionSomatic = rcvs[0]
+    assert rcv.clincal_impact_assertion_type is None
+    assert rcv.clinical_impact_clinical_significance is None
+    assert rcv.date_last_evaluated is None
+    assert rcv.entity_type == "rcv_accession_somatic"
+    assert rcv.id == "RCV000000012"
+    assert rcv.independent_observations is None
+    assert rcv.interpretation == "Pathogenic"
+    assert rcv.review_status == "criteria provided, single submitter"
+    assert rcv.statement_type == StatementType.GermlineClassification
+    assert rcv.submission_count is None
+    assert (
+        rcv.title
+        == "NM_014855.3(AP5Z1):c.80_83delinsTGCTGTAAACTGTAACTGTAAA (p.Arg27_Ile28delinsLeuLeuTer) AND Hereditary spastic paraplegia 48"
+    )
+    assert rcv.trait_set_id == "2"
+    assert rcv.variation_archive_id == "VCV000000002"
+    assert rcv.version == 5
